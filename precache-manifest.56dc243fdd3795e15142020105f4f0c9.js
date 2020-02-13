3e6a03fe55b3497090045e18a82c19c18754c9e6
Kacper Nowosielski (prophet-kn) <kacper.nowosielski@gmail.com>
2020-02-13 18:35:57 +0200

    Updates

---
+++ precache-manifest.56dc243fdd3795e15142020105f4f0c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970879c6f87801884f79694c3cf938c9",
+    "url": "/eurojackpot-generator/index.html"
+  },
+  {
+    "revision": "ffe6f44a17f43efe12c3",
+    "url": "/eurojackpot-generator/static/css/main.39b99a25.chunk.css"
+  },
+  {
+    "revision": "8b86a30ccd2827e6b911",
+    "url": "/eurojackpot-generator/static/js/2.1f7a2dbf.chunk.js"
+  },
+  {
+    "revision": "ffe6f44a17f43efe12c3",
+    "url": "/eurojackpot-generator/static/js/main.6571f5ce.chunk.js"
+  },
+  {
+    "revision": "4b2298d9c11a37fc5e5b",
+    "url": "/eurojackpot-generator/static/js/runtime~main.a8da8e99.js"
+  }
+]);
