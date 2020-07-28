fa16243e669644d7370ee4c2568a8b0df8565f65
Kacper Nowosielski <kacper.nowosielski@siili.com>
2020-07-28 11:13:34 +0300

    fixes n' updates

--- src/App.js
+++ src/App.js
@@ -58,41 +58,30 @@ class App extends Component {
   }
 
   generateButton() {
-    let btn = this.state.generate,
-        five = this.state.numbersFive,
-        two = this.state.numbersTwo
+    const five = this.state.numbersFive
+    const two = this.state.numbersTwo
 
-    if (btn === false) {
-      return (
-        <div className={'ejp-main-generate'} onClick={(e) => this.rollNumbers()}>
-          <svg width="200" height="200" viewBox="0 0 14 16"><path fillRule="evenodd" d="M8 9H6V4h2v5zm-2 3h2v-2H6v2zm6.33-2H10l1.5 1.5c-1.05 1.33-2.67 2.2-4.5 2.2A5.71 5.71 0 011.3 8c0-.34.03-.67.09-1H.08C.03 7.33 0 7.66 0 8c0 3.86 3.14 7 7 7 2.19 0 4.13-1.02 5.41-2.59L14 14v-4h-1.67zM1.67 6H4L2.5 4.5C3.55 3.17 5.17 2.3 7 2.3c3.14 0 5.7 2.56 5.7 5.7 0 .34-.03.67-.09 1h1.31c.05-.33.08-.66.08-1 0-3.86-3.14-7-7-7-2.19 0-4.13 1.02-5.41 2.59L0 2v4h1.67z"></path></svg>
+    return (
+      <div className={'ejp-main-generated'}>
+        <div className={'ejp-main-generated-numbers'}>
+          {five.sort((a, b) => a - b).map((e, i) => {
+            return (
+              <span key={i}>{e}</span>
+            )
+          })}
         </div>
-      )
-    }
-    else if (btn === true) {
-      return (
-        <div className={'ejp-main-generated'}>
-          <div className={'ejp-main-generated-numbers'}>
-            {five.sort((a, b) => a - b).map((e, i) => {
-              return (
-                <span key={i}>{e}</span>
-              )
-            })}
-          </div>
-          <div className={'ejp-main-generated-numbers'}>
-            {two.sort((a, b) => a - b).map((e, i) => {
-              return (
-                <span key={i}>{e}</span>
-              )
-            })}
-          </div>
-          <div className={'ejp-main-return'} onClick={(e) => this.cleanStates()}>
-            <svg width="100" height="100" viewBox="0 0 14 16"><path fillRule="evenodd" d="M8 9H6V4h2v5zm-2 3h2v-2H6v2zm6.33-2H10l1.5 1.5c-1.05 1.33-2.67 2.2-4.5 2.2A5.71 5.71 0 011.3 8c0-.34.03-.67.09-1H.08C.03 7.33 0 7.66 0 8c0 3.86 3.14 7 7 7 2.19 0 4.13-1.02 5.41-2.59L14 14v-4h-1.67zM1.67 6H4L2.5 4.5C3.55 3.17 5.17 2.3 7 2.3c3.14 0 5.7 2.56 5.7 5.7 0 .34-.03.67-.09 1h1.31c.05-.33.08-.66.08-1 0-3.86-3.14-7-7-7-2.19 0-4.13 1.02-5.41 2.59L0 2v4h1.67z"></path></svg>
-          </div>
+        <div className={'ejp-main-generated-numbers'}>
+          {two.sort((a, b) => a - b).map((e, i) => {
+            return (
+              <span key={i}>{e}</span>
+            )
+          })}
         </div>
-      )
-    }
-    
+        <div className={'ejp-main-return'} onClick={(e) => this.cleanStates()}>
+          <svg width="100" height="100" viewBox="0 0 14 16"><path fillRule="evenodd" d="M8 9H6V4h2v5zm-2 3h2v-2H6v2zm6.33-2H10l1.5 1.5c-1.05 1.33-2.67 2.2-4.5 2.2A5.71 5.71 0 011.3 8c0-.34.03-.67.09-1H.08C.03 7.33 0 7.66 0 8c0 3.86 3.14 7 7 7 2.19 0 4.13-1.02 5.41-2.59L14 14v-4h-1.67zM1.67 6H4L2.5 4.5C3.55 3.17 5.17 2.3 7 2.3c3.14 0 5.7 2.56 5.7 5.7 0 .34-.03.67-.09 1h1.31c.05-.33.08-.66.08-1 0-3.86-3.14-7-7-7-2.19 0-4.13 1.02-5.41 2.59L0 2v4h1.67z"></path></svg>
+        </div>
+      </div>
+    )
   }
 
   render() {
